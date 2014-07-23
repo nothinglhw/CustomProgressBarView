6ab873fe238c7e19a8d899ef2b588cad5891baf0
nothinglhw <nothinglhw@nothinglhwtekiiMac.local>
2014-07-23 21:24:03 +0800

    Initial Commit

---
+++ customProgressBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  customProgressBar
+//
+//  Created by nothinglhw on 14-7-23.
+//  Copyright (c) 2014年 nothinglhw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ customProgressBar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  customProgressBar
+//
+//  Created by nothinglhw on 14-7-23.
+//  Copyright (c) 2014年 nothinglhw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
